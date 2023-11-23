e95f636751b73ca6c85e354487de6f6ff5e084b5
Kaleb Asfaw <122475977+kaleb-asfaw@users.noreply.github.com>
2023-11-22 20:30:45 -0500

    Update content.js

--- ad-skip/content.js
+++ ad-skip/content.js
@@ -1,12 +1,39 @@
+function accelerate(){
+  const videoElement = document.querySelector('video')
+  if (videoElement) {
+    videoElement.playbackRate = 15;
+    videoElement.muted = true;
+  }
+}
+function restoreNormal(){
+  const videoElement = document.querySelector('video')
+  if (videoElement){
+    videoElement.playbackRate = 1;
+    videoElement.muted = false;
+  }
+}
+function isAdUnskippable(){
+  return !!document.querySelector(".ytp-ad-preview, .ytp-ad-preview-slot")
+}
+
+
 function clickSkipAdButton() {
-    const skipButton = document.querySelector(".ytp-ad-skip-button, .ytp-ad-overlay-close-button, .ytp-ad-skip-button-slot"); // patch for the Chrome 120 Beta update
+  //For already 'skippable' ads (on these sides, everything is skippable hehe)
+  const skipButton = document.querySelector(".ytp-ad-skip-button, .ytp-ad-overlay-close-button, .ytp-ad-skip-button-slot");
     if (skipButton) {
       skipButton.click();
     }
   }
-
   const checkInterval = 1000; // every 1000ms, a check will occur
-  
+
   const adSkipChecker = setInterval(() => {
-    clickSkipAdButton();
- }, checkInterval);
+    const skipButton = document.querySelector(".ytp-ad-skip-button, .ytp-ad-overlay-close-button, .ytp-ad-skip-button-slot")
+    if (skipButton){
+      clickSkipAdButton();
+    } else if (isAdUnskippable()){
+      accelerate();
+    } else {
+      restoreNormal();
+    }
+    
+  }, checkInterval);
