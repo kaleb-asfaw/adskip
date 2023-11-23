714da89ff3b4e5d20f0bb31b506c44893695a2ea
Kaleb Asfaw <122475977+kaleb-asfaw@users.noreply.github.com>
2023-11-23 13:59:26 -0500

    Update content.js (2.0.2 patch for speeding up/muting videos)

--- ad-skip/content.js
+++ ad-skip/content.js
@@ -8,30 +8,38 @@ function clickSkipAdButton() {
       skipButton.click();
     }
   }
-function accelerate(){
-  const videoElement = document.querySelector('video')
-  if (videoElement) {
-    videoElement.playbackRate = 15;
-    videoElement.muted = true;
+  function accelerate(){
+    const videoElement = document.querySelector('video');
+    if (videoElement) {
+      videoElement.playbackRate = 15;
+      videoElement.muted = true;
+    }
   }
-}
-function restoreNormal(){
-  const videoElement = document.querySelector('video')
-  if (videoElement){
-    videoElement.playbackRate = 1;
-    videoElement.muted = false;
+  function restoreNormal(){
+    const videoElement = document.querySelector('video');
+    if (videoElement){
+      videoElement.playbackRate = 1;
+      videoElement.muted = false;
+    }
   }
-}
   const checkInterval = 1000; // every 1000ms, a check will occur
+  function isAdPlaying() {
+    return !!document.querySelector('.ytp-ad-preview, .ytp-ad-preview-slot');
+  }
 
   const adSkipChecker = setInterval(() => {
-    const skipButton = document.querySelector(".ytp-ad-skip-button, .ytp-ad-overlay-close-button, .ytp-ad-skip-button-slot")
+    const skipButton = document.querySelector(".ytp-ad-skip-button, .ytp-ad-overlay-close-button, .ytp-ad-skip-button-slot");
+    const adCurrentlyPlaying = isAdPlaying();
+
     if (skipButton){
       clickSkipAdButton();
-    } else if (isAdUnskippable()){
+      wasAdPlaying = true;
+    } else if (adCurrenlyPlaying || isAdUnskippable()){
       accelerate();
-    } else {
+      wasAdPlaying = true;
+    } else if (wasAdPlaying && !adCurrenlyPlaying) { 
       restoreNormal();
+      wasAdPlaying = false;
     }
     
   }, checkInterval);
