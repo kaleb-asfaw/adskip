18c016054f8e7da2bf84e3a12e6b73ec45e071b8
Kaleb Asfaw <122475977+kaleb-asfaw@users.noreply.github.com>
2023-11-22 20:31:18 -0500

    Update content.js

--- ad-skip/content.js
+++ ad-skip/content.js
@@ -1,3 +1,13 @@
+function isAdUnskippable(){
+  return !!document.querySelector(".ytp-ad-preview, .ytp-ad-preview-slot")
+}
+function clickSkipAdButton() {
+  //For already 'skippable' ads (on these sides, everything is skippable hehe)
+  const skipButton = document.querySelector(".ytp-ad-skip-button, .ytp-ad-overlay-close-button, .ytp-ad-skip-button-slot");
+    if (skipButton) {
+      skipButton.click();
+    }
+  }
 function accelerate(){
   const videoElement = document.querySelector('video')
   if (videoElement) {
@@ -12,18 +22,6 @@ function restoreNormal(){
     videoElement.muted = false;
   }
 }
-function isAdUnskippable(){
-  return !!document.querySelector(".ytp-ad-preview, .ytp-ad-preview-slot")
-}
-
-
-function clickSkipAdButton() {
-  //For already 'skippable' ads (on these sides, everything is skippable hehe)
-  const skipButton = document.querySelector(".ytp-ad-skip-button, .ytp-ad-overlay-close-button, .ytp-ad-skip-button-slot");
-    if (skipButton) {
-      skipButton.click();
-    }
-  }
   const checkInterval = 1000; // every 1000ms, a check will occur
 
   const adSkipChecker = setInterval(() => {
